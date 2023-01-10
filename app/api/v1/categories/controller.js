7ecd2b0987445d82d4a176a36c48d3c0689c91c1
sendhyrama <sendhyrama@gmail.com>
2023-01-10 13:10:29 +0700

    add services categories

--- app/api/v1/categories/controller.js
+++ app/api/v1/categories/controller.js
@@ -1,83 +1,78 @@
-const Categories = require('./model');
-
-const create = async ( req , res , next) => {
-    try {
-        const { name } = req.body;
-        const result = await Categories.create({ name });
-        res.status(201).json ({
-            data: result,
-        });
-    }catch (err) {
-        next (err);
-    }
-};
-
-const index = async (req, res, next) => {
-    try {
-        const result = await Categories.find().select('_id name');
-        res.status(200).json ({
-            data: result,
-        });
-    } catch(err) {
-        next(err);
-    }
-};
-
-const find = async (req, res, next) => {
-    try {
-        const {id} = req.params;
-        const result = await Categories.findOne({ _id: id });
-        
-        if (!result) {
-            return res.status(404).json({ message: 'Id Categories tidak ditemukan '});
-        }
-        
-        res.status(200).json ({
-            data: result,
-        });
-    } catch(err) {
-        next(err);
-    }
-};
-
-const update = async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const { name } = req.body;
+// import services categories
+const {
+    getAllCategories,
+    getOneCategories,
+    updateCategories,
+    createCategories,
+    deleteCategories,
+  } = require('../../../services/mongoose/categories');
   
-      // cari dan update categories berdasarkan field _id
-      const result = await Categories.findOneAndUpdate(
-        { _id: id },
-        { name },
-        { new: true, runValidators: true } // menampilkan data baru dan menjalankan validation
-      );
+  const { StatusCodes } = require('http-status-codes');
   
-      res.status(200).json({
+  const create = async (req, res) => {
+    try {
+      const result = await createCategories(req);
+  
+      res.status(StatusCodes.CREATED).json({
         data: result,
       });
     } catch (err) {
       next(err);
     }
   };
-
-  const destroy = async (req, res, next) => {
+  
+  const index = async (req, res) => {
     try {
-      const { id } = req.params;
-      
-      // cari dan hapus categories berdasakan field _id
-      const result = await Categories.findByIdAndRemove(id);
-      res.status(200).json({
+      const result = await getAllCategories(req);
+  
+      res.status(StatusCodes.OK).json({
         data: result,
       });
     } catch (err) {
       next(err);
     }
   };
-
-module.exports = {
+  
+  const find = async (req, res) => {
+    try {
+      const result = await getOneCategories(req);
+  
+      res.status(StatusCodes.OK).json({
+        data: result,
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+  
+  const update = async (req, res) => {
+    try {
+      const result = await updateCategories(req);
+  
+      res.status(StatusCodes.OK).json({
+        data: result,
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+  
+  const destroy = async (req, res) => {
+    try {
+      const result = await deleteCategories(req);
+  
+      res.status(StatusCodes.OK).json({
+        data: result,
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+  
+  module.exports = {
     index,
-    create,
     find,
     update,
-    destroy
-};
+    destroy,
+    create,
+  };
