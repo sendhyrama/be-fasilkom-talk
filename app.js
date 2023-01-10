a25d6b01b3c74f891054559abc093f63d4f584ad
sendhyrama <sendhyrama@gmail.com>
2023-01-10 12:35:46 +0700

    add custom error

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const logger = require('morgan');
 const app = express();
 
 const categoriesRouter = require('./app/api/v1/categories/router');
-const v1 = '/api/v1/cms';
 
+// middlewares
+const notFoundMiddleware = require('./app/middlewares/not-found');
+const handleErrorMiddleware = require('./app/middlewares/handler-error');
+
+const v1 = '/api/v1/cms';
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,12 +19,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req , res) => {
-    res.status(200).json ({
-        message: 'welcome to api fasilkom talk',
-    });
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to api semina',
+  });
 });
 
-app.use (v1, categoriesRouter);
+app.use(v1, categoriesRouter);
+
+// middlewares
+app.use(notFoundMiddleware);
+app.use(handleErrorMiddleware);
 
 module.exports = app;
